0ab6ba02468545adfbe2902495a0a007ce415c28
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2012-08-09 15:31:12 +0700

    Допили до рабочего состояния + оптимизации

--- script.R
+++ script.R
@@ -6,8 +6,8 @@ library(multicore)
 dists <- c("NO", "LOGNO", "GA", "WEI", "IG", "exGAUS")
 
 ## Функция расчитывает AIC для разных типов распредления
-## Возвращает таблицу: Распределение - AIC
-define.dist <- function(x) {
+## Возвращает AIC для каждого типа распределения
+define.dist <- function(x, distname = FALSE) {
   rw <- NULL
   x <- as.numeric(na.omit(x))
   for (i in dists) {
@@ -15,20 +15,19 @@ define.dist <- function(x) {
     aic <- round(aic, digits = 2)
     rw <- c(rw, aic)
   }
-  return(rw)
+  if (distname == TRUE) {
+    distname <- dists[which.min(rw)]
+    return(distname)
+  }
+  else {
+    rw <- matrix(rw)
+    rownames(rw) <- dists
+    return(rw)
+  }
 }
 
-rw <- data.frame(mclapply(empdata, FUN = define.dist, mc.silent = TRUE))
-rownames(rw) <- dists
-# Печать результатов
-
-
-# Наиболее подходящий тип распределения
-dist <- dists[(sapply(rw, FUN = which.min))]
-#print(dist)
-
 ## Функция для бутстрапа, расчитывает параметры заданного распределения (mu, sigma)
-fit.dist <- function(data, indices, dist) {rw
+fit.dist <- function(data, indices, dist) {
   x <- as.numeric(na.omit(data[indices]))
   fit.out <- gamlss(x ~ 1, family = dist)
   mu <- as.numeric(fit.out$mu.coefficients)
@@ -43,20 +42,25 @@ boot.dist <- function(x, dist) {
 # Задаём параметры для геренатора (для воспроизводимости результатов)
   set.seed(1234)
 # Расчитываем параметры распределения
-  boot.out <- boot(x, statistic=fit.dist, R=1000, parallel = "multicore", dist = dist)
+  boot.out <- boot(x, statistic=fit.dist, dist = dist, R=1000, parallel = "multicore")
   mu <- boot.out$t0[1]
   sigma <- boot.out$t0[2]
   conf.mu <- boot.ci(boot.out, index = 1, type = "bca")$bca[4:5]
   conf.sigma <- boot.ci(boot.out, index = 2, type = "bca")$bca[4:5]
   result <- c(mu, conf.mu, sigma, conf.sigma)
-  return((result))
+  return(result)
 }
 
-distparams  <- data.frame(mclapply(empdata[,1:3], FUN = boot.dist, dist = LOGNO, mc.silent = TRUE))
-rownames(distparams) <- c("mu", "mu - 95% low", "mu - 95 up", "sigma",  "sigma - 95% low", "sigma - 95 up")
+# Таблица значений AIC для каждого типа распределения
+dist.aic <- data.frame(mclapply(empdata[,1:3], FUN = define.dist, mc.silent = TRUE))
+rownames(dist.aic) <- dists
 
-estMean <- rowMeans(distparams)[1]
-estSD <- rowMeans(distparams)[3]
+# Таблица параметров распределния + доверительные интервалы
+dist.params  <- data.frame(mclapply(empdata[,1:3], FUN = function(x) boot.dist(x, dist = define.dist(x, distname = TRUE)),  mc.silent = TRUE))
+rownames(dist.params) <- c("mu", "mu - 95% low", "mu - 95 up", "sigma",  "sigma - 95% low", "sigma - 95 up")
+
+estMean <- rowMeans(dist.params)[1]
+estSD <- rowMeans(dist.params)[3]
 # Печать результатов
-print(rw)
-print(distparams)
+print(dist.aic)
+print(dist.params)
